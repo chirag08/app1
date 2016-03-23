001a0421ebbdf19418bca55afbe0ec68e5ae76b1
Chirag <chirag.student@gmail.com>
2016-03-23 17:13:23 +0530

    Sign up Complete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+	def gravator_for(user)
+		gravator_id = Digest::MD5::hexdigest(user.email.downcase)
+		gravator_url= "https://secure.gravatar.com/avatar/#{gravator_id}"
+		image_tag(gravator_url,alt: user.name,class: "gravator card-panel")
+	end
 end
